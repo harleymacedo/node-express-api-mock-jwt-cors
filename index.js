2b6e45318b3bbd3523b72d340628038faa0d4296
harleymacedo <harley.ip@gmail.com>
2022-10-25 10:29:43 -0300

    Funcionando de forma padrão

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+const professores = [
+    {nome: 'Mario', area: 'Area1'},
+    {nome: 'Luigi', area: 'Area2'},
+    {nome: 'Alex', area: 'Area3'}
+];
+
+const verificarJWT = (req, res, next) => {
+    const token = req.body.token;
+    if (!token) {
+        res.json({logado: false, mensagem: 'Token não foi enviado.'});
+    }
+    jwt.verify(token, process.env.APP_KEY, (err, decoded) => {
+        if (err) {
+            res.json({locado: false, mensagem: 'Falha na autenticação'});
+        }
+    });
+    next();
+}
+
+app.post('/logar', (req, res) => {
+    try {
+        const {usuario, senha} = req.body;
+        console.log(usuario, process.env.USUARIO, senha, process.env.SENHA);
+        if (usuario === process.env.USUARIO && senha === process.env.SENHA) {
+            let novoToken = jwt.sign({usuario}, process.env.APP_KEY, {expiresIn: 9000});
+            res.json({logado: true, token: novoToken});
+        }
+        res.json({logado: false, mensagem: 'Usuário ou senha errados.'});        
+    } catch (error) {
+        res.json({logado: false, mensagem: 'Erro durante o login.'});        
+    }
+});
+
+//Rota sem verificação de token
+app.get('/professor/todos', (req, res) => {
+    try {
+        res.json(professores);
+    } catch (error) {
+        res.json({erro: true, mensagem: 'Erro na consulta'});
+    }
+});
+
+app.post('/professor', verificarJWT, (req, res) => {
+    try {
+        const {nome, area, token} = req.body;
+        const novoProf = {nome, area};
+        professores.push(novoProf);
+        res.json({mensagem: 'Novo professor cadastrado.'});
+    } catch (error) {
+        res.json({erro: true, mensagem: 'Não foi possível realizar cadastro'});
+    }
+});
+
+app.listen(process.env.PORT || 3000);
